21ddb66e003f3ad4eb519d1f9121419d7d18571c
Armin Kirchner <post.armin@gmail.com>
2020-01-02 21:19:37 +0900

    Issue form design with date picker
    Issue list views with optional due date.

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,7 +3,9 @@
 
 import { Application } from 'stimulus';
 import { definitionsFromContext } from 'stimulus/webpack-helpers';
+import Flatpickr from 'stimulus-flatpickr';
 
 const application = Application.start();
 const context = require.context('controllers', true, /_controller\.js$/);
 application.load(definitionsFromContext(context));
+application.register('flatpickr', Flatpickr);
