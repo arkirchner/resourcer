a19ecef8c265ff325d873f7c5fd4b3dcd624166c
Armin Kirchner <post.armin@gmail.com>
2020-01-13 17:09:48 +0900

    RSR-4 Chrome with disable-dev-shm-usage for CI

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,7 +3,9 @@ require_relative "./support/auth_system_test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   include AuthSystemTestHelper
-  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]do |driver_options|
+    driver_options.add_argument('disable-dev-shm-usage')
+  end
 
   def setup
     super
