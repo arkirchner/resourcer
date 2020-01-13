463d59d3da7dd5081566f586c6fcb90ea627f6eb
Armin Kirchner <post.armin@gmail.com>
2020-01-14 01:16:18 +0900

    RSR-5 User dashboard shows only projects related

--- test/system/projects_test.rb
+++ test/system/projects_test.rb
@@ -13,4 +13,21 @@ class ProjectsTest < ApplicationSystemTestCase
 
     assert_text "New project was created."
   end
+
+  test "only projects related to the a user are displayed" do
+    sign_up_with_github
+    FactoryBot.create :project, name: "This is not my project!"
+
+    click_on "Add project"
+
+    fill_in "Name", with: "This is a my new project."
+    fill_in "Key", with: "MYP"
+
+    click_on "Create"
+
+    click_on "Dashboard"
+
+    assert_text "This is a my new project."
+    assert_no_text "This is not my project!"
+  end
 end
