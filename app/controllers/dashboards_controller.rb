528bc93505071ea5d635dab17e71a35fbf959b37
Armin Kirchner <post.armin@gmail.com>
2020-03-17 21:27:14 +0900

    RSR-13 Issues scoped by due date

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,20 +2,50 @@ class DashboardsController < ApplicationController
   def show
     @histories =
       History.related_to_member(@current_member).order(changed_at: :desc)
-    @issues = if created_by_member?
-                current_member.created_issues
-              else
-                current_member.assigned_issues
-              end
+    @issues =
+      if my_issues_params[:created] == "true"
+        current_member.created_issues
+      elsif my_issues_params[:assigned] == "true"
+        current_member.assigned_issues
+      end.then do |relation|
+        if my_issues_params[:all] == "true"
+          relation
+        elsif my_issues_params[:four_day] == "true"
+          relation.where(due_at: Date.today..4.days.from_now)
+        elsif my_issues_params[:today] == "true"
+          relation.where(due_at: Date.today)
+        elsif my_issues_params[:overdue] == "true"
+          relation.where(Issue.arel_table[:due_at].lt(Date.today))
+        end
+      end
+
     @my_issues_params = my_issues_params
   end
 
   private
 
   def my_issues_params
-    return { assigned: "true" } if params[:my_issue].blank?
+    if params[:my_issue].blank?
+      return(
+        {
+          assigned: "true",
+          created: "false",
+          all: "true",
+          four_day: "false",
+          today: "false",
+          overdue: "false",
+        }
+      )
+    end
 
-    params.require(:my_issue).permit(:assigned, :created, :all, :four_day, :today, :overdue)
+    params.require(:my_issue).permit(
+      :assigned,
+      :created,
+      :all,
+      :four_day,
+      :today,
+      :overdue,
+    )
   end
 
   def created_by_member?
--- app/helpers/dashboad_helper.rb
+++ app/helpers/dashboad_helper.rb
@@ -1,15 +1,7 @@
 module DashboadHelper
   def my_issues_selection(my_issues_params, select_params)
     tag.div do
-      {
-        assigned: true,
-        created: false,
-        all: true,
-        four_day: false,
-        today: false,
-        overdue: false,
-      }.merge(my_issues_params)
-        .merge(select_params).each do |key, value|
+      my_issues_params.merge(select_params).each do |key, value|
         concat hidden_field_tag("my_issue[#{key}]", value)
       end
     end
