cbf9dd0e4162fd5a20d9f64d21cb02cafdb81746
Armin Kirchner <post.armin@gmail.com>
2020-05-06 17:08:15 +0900

    Cache histories and optimized dashboard queries

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,33 +1,32 @@
 class DashboardsController < ApplicationController
   def show
     @histories =
-      History.related_to_member(@current_member).order(changed_at: :desc)
-        .includes(
-          :associated_issue,
-          :member,
-          issue: %i[from_assignee to_assignee],
-        ).limit(20)
-    @issues =
-      if my_issues_params[:created] == "true"
-        current_member.created_issues.incomplete
-      elsif my_issues_params[:assigned] == "true"
-        current_member.assigned_issues.incomplete
-      end.then do |relation|
-        if my_issues_params[:all] == "true"
-          relation
-        elsif my_issues_params[:four_day] == "true"
-          relation.where(due_at: Date.today..4.days.from_now)
-        elsif my_issues_params[:today] == "true"
-          relation.where(due_at: Date.today)
-        elsif my_issues_params[:overdue] == "true"
-          relation.where(Issue.arel_table[:due_at].lt(Date.today))
-        end
-      end.includes(:project)
-
+      History.related_to_member(current_member).order(changed_at: :desc).limit(
+        20,
+      )
+    @issues = issues
     @my_issues_params = my_issues_params
   end
 
   private
+
+  def issues
+    if my_issues_params[:created] == "true"
+      current_member.created_issues.incomplete
+    elsif my_issues_params[:assigned] == "true"
+      current_member.assigned_issues.incomplete
+    end.then do |relation|
+      if my_issues_params[:all] == "true"
+        relation
+      elsif my_issues_params[:four_day] == "true"
+        relation.where(due_at: Date.today..4.days.from_now)
+      elsif my_issues_params[:today] == "true"
+        relation.where(due_at: Date.today)
+      elsif my_issues_params[:overdue] == "true"
+        relation.where(Issue.arel_table[:due_at].lt(Date.today))
+      end
+    end.joins(:project).select("issues.*, projects.key AS project_key")
+  end
 
   def my_issues_params
     if params[:my_issue].blank?
@@ -55,10 +54,5 @@ class DashboardsController < ApplicationController
 
   def created_by_member?
     params[:issues_by_member].present?
-  end
-
-  def current_member
-    @current_member ||=
-      Member.includes(:projects).find_by(id: session[:member_id])
   end
 end
